464acca51adc2247c5d716f0266925b3492359e3
William Antony <william@pathadan.com>
2020-04-30 13:29:33 -0500

    Created HeaderSearchIcon Component; Not connected with redux store

---
+++ src/shared/components/HeaderSearchIcon/HeaderSearchIcon.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HeaderSearchIcon.css";
+
+const HeaderSearchIcon = function(props) {
+  return (
+    <div className="HeaderSearchIcon">
+      <div className="HeaderSearchIcon__icon"></div>
+    </div>
+  );
+};
+
+export default HeaderSearchIcon;
