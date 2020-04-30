1faafe7eb4d0f99c60386b9c6620fd619c7168df
William Antony <william@pathadan.com>
2020-04-30 13:45:35 -0500

    Created NotFoundError Page Component

---
+++ src/shared/pages/Error/NotFoundError.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Page from "../../components/Page/Page";
+import Header from "../../components/Header/Header";
+import "./NotFoundError.css";
+
+const NotFoundError = function(props) {
+  return (
+    <Page>
+      <Header theme="light" />
+      <div className="NotFoundError__content">
+        <div className="NotFoundError__text">
+          <div className="NotFoundError__text__error-title">404</div> 
+          <div className="NotFoundError__text__error-description">
+            This page doesn't exist yet!
+          </div>
+        </div>
+        <div className="NotFoundError__clear__bg"></div>
+      </div>
+    </Page>
+  );
+};
+
+export default NotFoundError;
--- src/shared/ui/pages/error/NotFoundError.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const NotFoundError = function(props) {
-  return <div>404 - Not Found Error</div>;
-};
-
-export default NotFoundError;
