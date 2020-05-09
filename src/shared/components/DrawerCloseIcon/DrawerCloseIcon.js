69b5f6026b5cc94a001b38bb747abcb7a401e690
William Antony <william@pathadan.com>
2020-05-09 15:53:20 -0500

    Created DrawerCloseIcon Component

---
+++ src/shared/components/DrawerCloseIcon/DrawerCloseIcon.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./DrawerCloseIcon.css";
+
+const DrawerCloseIcon = function({ onClick }) {
+  return (
+    <div className="DrawerCloseIcon" onClick={ onClick }>
+      <div className="DrawerCloseIcon__holder">
+        <div className="DrawerCloseIcon__icon">
+          <div className="DrawerCloseIcon__line"></div>
+          <div className="DrawerCloseIcon__line"></div>
+          <div className="DrawerCloseIcon__line"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DrawerCloseIcon;
