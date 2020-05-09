d95cf82e69afeb2099fbde9d961a7a4267173efe
William Antony <william@pathadan.com>
2020-05-09 15:44:51 -0500

    Refactored HeaderSearchIcon Component

--- src/shared/components/HeaderSearchIcon/HeaderSearchIcon.js
+++ src/shared/components/HeaderSearchIcon/HeaderSearchIcon.js
@@ -1,23 +1,12 @@
 import React from "react";
-import { connect } from "react-redux";
-import { showSearchBar } from "../../redux/actions";
 import "./HeaderSearchIcon.css";
 
-const HeaderSearchIcon = function({ showSearchBar }) {
+const HeaderSearchIcon = function({ onClick }) {
   return (
-    <div className="HeaderSearchIcon" onClick={ showSearchBar }>
+    <div className="HeaderSearchIcon" onClick={ onClick }>
       <div className="HeaderSearchIcon__icon"></div>
     </div>
   );
 };
 
-const mapStateToProps = state => {
-  return {};
-};
-
-const mapDispatchToProps = {
-  showSearchBar
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(HeaderSearchIcon);
-
+export default HeaderSearchIcon;
