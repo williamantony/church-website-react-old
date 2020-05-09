8c25a4e7f07e22ad7954d6e0ae602111a8255781
William Antony <william@pathadan.com>
2020-05-09 15:49:37 -0500

    Created SearchDrawer Component

---
+++ src/shared/components/SearchDrawer/SearchDrawer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import SearchBox from "../SearchBox/SearchBox";
+import "./SearchDrawer.css";
+
+const SearchDrawer = function(props) {
+  return (
+    <div className="SearchDrawer">
+      <div className="wrapper wrapper--800px">
+        <div className="SearchDrawer__holder">
+          <SearchBox autofocus={ true } />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchDrawer;
