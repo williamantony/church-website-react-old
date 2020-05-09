9375907ebefd3cc14d8efc48b198ede1455c900a
William Antony <william@pathadan.com>
2020-05-09 16:09:45 -0500

    Refactored NotFoundError page Component

--- src/shared/pages/Error/NotFoundError.js
+++ src/shared/pages/Error/NotFoundError.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Page from "../../components/Page/Page";
 import Header from "../../components/Header/Header";
+import PrimaryHeader from "../../components/PrimaryHeader/PrimaryHeader";
 import "./NotFoundError.css";
 
 const NotFoundError = function(props) {
   return (
-    <Page>
-      <Header theme="light" />
+    <Page name="404">
+      <Header theme="light" component={ <PrimaryHeader logo="ipa" /> } />
       <div className="NotFoundError__content">
         <div className="NotFoundError__text">
           <div className="NotFoundError__text__error-title">404</div> 
