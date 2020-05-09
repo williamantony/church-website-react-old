527166f3a06dd80f7aea2b3a05d4ca19d4ac4ea3
William Antony <william@pathadan.com>
2020-05-09 16:11:03 -0500

    Added routes to routes.list.js

--- src/shared/router/routes.list.js
+++ src/shared/router/routes.list.js
@@ -1,11 +1,29 @@
 import Home from "../pages/Home/Home";
 import NotFoundError from "../pages/Error/NotFoundError";
+import About from "../pages/About/About";
+import Leadership from "../pages/About/Leadership";
+import Beliefs from "../pages/About/Beliefs";
 
 const routes = [
   {
     exact: true,
     path: "/",
     component: Home,
+  },
+  {
+    exact: true,
+    path: "/about",
+    component: About,
+  },
+  {
+    exact: true,
+    path: "/about/leadership",
+    component: Leadership,
+  },
+  {
+    exact: true,
+    path: "/about/beliefs",
+    component: Beliefs,
   },
   {
     component: NotFoundError,
