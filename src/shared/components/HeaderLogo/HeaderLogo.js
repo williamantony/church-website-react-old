83b5a1f3999720ba9635599c4840a84b662574bd
William Antony <william@pathadan.com>
2020-05-09 15:45:12 -0500

    Refactored HeaderLogo Component

--- src/shared/components/HeaderLogo/HeaderLogo.js
+++ src/shared/components/HeaderLogo/HeaderLogo.js
@@ -1,20 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./HeaderLogo.css";
 
 const HeaderLogo = function({ logo = "none", size = "medium", type = "name" }) {
-  const LogoClassName = [
-    "HeaderLogo",
-    ` HeaderLogo--logo-${ logo }`,
-    ` HeaderLogo--size-${ size }`,
-    ` HeaderLogo--type-${ type }`
-  ].join("");
+  const classes = {
+    logo: ` HeaderLogo--logo-${ logo }`,
+    size: ` HeaderLogo--size-${ size }`,
+    type: ` HeaderLogo--type-${ type }`,
+  };
 
   return (
-    <div className={ LogoClassName }>
-      <div className="HeaderLogo__image">
-        <div className="HeaderLogo__image__img HeaderLogo__image__img--light"></div>
-        <div className="HeaderLogo__image__img HeaderLogo__image__img--dark"></div>
-      </div>
+    <div className={ `HeaderLogo${ classes.logo }${ classes.size }${ classes.type }` }>
+      <Link to="/">
+        <div className="HeaderLogo__image">
+          <div className="HeaderLogo__image__img HeaderLogo__image__img--light"></div>
+          <div className="HeaderLogo__image__img HeaderLogo__image__img--dark"></div>
+        </div>
+      </Link>
     </div>
   );
 };
