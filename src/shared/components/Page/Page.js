0f029737f833f585851539214aa46884a383811d
William Antony <william@pathadan.com>
2020-05-13 11:00:06 -0500

    Added setPageName() to Page Component

--- src/shared/components/Page/Page.js
+++ src/shared/components/Page/Page.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { closeDrawer } from "../../redux/actions";
+import { setPageName, closeDrawer } from "../../redux/actions";
 import Drawer from "../Drawer/Drawer";
 import "./Page.css";
 
 class Page extends Component {
+  constructor(props) {
+    super(props);
+    props.setPageName(props.name);
+  }
+
   componentDidMount() {
     window.scrollTo(0, 0);
   }
@@ -12,29 +17,26 @@ class Page extends Component {
   render() {
     const { name, isActive, isScrollable } = this.props;
 
-    if (isScrollable)
-      document.body.classList.remove("no-scroll");
+    if (isScrollable) document.body.classList.remove("no-scroll");
     else document.body.classList.add("no-scroll");
 
     const classes = {
-      name: ` Page--${ name }`,
-      active: ` Page--${ isActive ? "active" : "inactive" }`
+      name: ` Page--${name}`,
+      active: ` Page--${isActive ? "active" : "inactive"}`,
     };
 
     return (
       <div className="wrapper wrapper--outer">
-        <div className={ `Page${ classes.name }${ classes.active }` }>
+        <div className={`Page${classes.name}${classes.active}`}>
           <Drawer />
-          <div className="Page__content">
-            { this.props.children }
-          </div>
+          <div className="Page__content">{this.props.children}</div>
         </div>
       </div>
     );
   }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   const { isActive, isScrollable } = state.page;
   return {
     isActive,
@@ -43,6 +45,7 @@ const mapStateToProps = state => {
 };
 
 const mapDispatchToProps = {
+  setPageName,
   closeDrawer,
 };
 
