4576fff242a4a4805f1de230b5ae5d3e34dc0937
William Antony <william@pathadan.com>
2020-05-15 10:44:56 -0500

    Added props to Button

--- src/shared/components/Button/Button.js
+++ src/shared/components/Button/Button.js
@@ -2,24 +2,24 @@ import React from "react";
 import { luminosity } from "../../utils/color";
 import "./Button.css";
 
-const Button = function({
+const Button = function ({
   type = "solid",
   hoverType = "solid",
   size = "medium",
   bgColor = "#FFFFFF",
   colorType,
-  text
+  text,
+  props = {},
 }) {
-  
-  const darkness = luminosity(bgColor).value < 0.60 ? "dark" : "light";
+  const darkness = luminosity(bgColor).value < 0.6 ? "dark" : "light";
   colorType = colorType ? colorType : darkness;
 
   const classList = [
     `Button`,
-    `Button--${ type }`,
-    `Button--hover-${ hoverType }`,
-    `Button--size-${ size }`,
-    `Button--color-${ colorType }`
+    `Button--${type}`,
+    `Button--hover-${hoverType}`,
+    `Button--size-${size}`,
+    `Button--color-${colorType}`,
   ].join(" ");
 
   const style = {
@@ -27,11 +27,9 @@ const Button = function({
   };
 
   return (
-    <button className={ classList } style={ style }>
+    <button className={classList} style={style} {...props}>
       <div className="Button__bg"></div>
-      <div className="Button__text">
-        { text }
-      </div>
+      <div className="Button__text">{text}</div>
     </button>
   );
 };
