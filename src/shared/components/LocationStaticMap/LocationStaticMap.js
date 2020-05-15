2ceff4eace5fc14678eed32cb81d41f5e5489aca
William Antony <william@pathadan.com>
2020-05-15 10:22:41 -0500

    Created LocationStaticMap Component

---
+++ src/shared/components/LocationStaticMap/LocationStaticMap.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./LocationStaticMap.css";
+
+const LocationStaticMap = function (props) {
+  return (
+    <div className="LocationStaticMap">
+      <div className="LocationStaticMap__map"></div>
+      <div className="LocationStaticMap__overlay">
+        <div className="LocationStaticMap__marker">
+          <div className="LocationStaticMap__marker__circle"></div>
+          <div className="LocationStaticMap__marker__circle"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LocationStaticMap;
